98d87af90943e55b70b2d04c6338bb1fb3191ba7
Jeffersonmk <jefferson20042011@protonmail.com>
2024-04-19 17:14:26 -0300

    modified: index.html and main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
     $('header button').click(function() {
         $('form').slideDown();
     })
@@ -7,7 +8,19 @@
     })
 
     $('form').on('submit', function(e) {
-        e.preventDefault();
-        console.log("submit");
+        e.preventdDefault();
+        const enderecoNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none;"></li>');
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+        $(`
+        <div class="overlay-imagem-link">
+            <a href ="${enderecoNovaImagem}" target ="_blank" title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
     })
-
+})
